462450705d688136b8c8faa4c8b258cdb52fe0f1
trongdinh07@gmail.com <Trong190724>
2024-10-01 17:56:03 +0700

    fix: delete

---
+++ API/src/utils/constants.js
@@ -0,0 +1,31 @@
+export const WHITELIST_DOMAINS = [];
+
+export const DEFAULT_PASSWORD = "Test@123";
+
+export const REMINDER_TYPES = {
+  PUBLIC: "public",
+  PRIVATE: "private",
+};
+
+export const STATUS = {
+  ACTIVE: "active",
+  INACTIVE: "inactive",
+};
+
+export const GENDER = {
+  MALE: "male",
+  FEMALE: "female",
+  OTHER: "other",
+};
+
+export const INVITE_STATUS = {
+  PENDING: "pending",
+  ACCEPTED: "accepted",
+  REJECTED: "rejected",
+};
+
+export const USER_ROLE = {
+  PATIENT: "patient",
+  DOCTOR: "doctor",
+  ADMIN: "admin",
+};
